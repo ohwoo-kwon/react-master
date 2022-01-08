8c22d436e1986214a386b9ce7d211e80e08ce241
ohwoo <kwonow1@gmail.com>
2022-01-08 16:22:12 +0900

    2.7 Themes

--- src/App.js
+++ src/App.js
@@ -1,49 +1,22 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const rotate360 = keyframes`
-  0% {
-    transform: rotate(0);
-    border-radius:0px;
-  }
-  50% {
-    transform:rotate(360deg);
-    border-radius:100px;
-  }
-  100% {
-    transform:rotate(0deg);
-    border-radius:0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: blue;
-  display: flex;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-  animation: ${rotate360} 5s linear infinite;
-  ${Emoji} {
-    &:hover {
-      font-size: 98px;
-    }
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>💚</Emoji>
-      </Box>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
