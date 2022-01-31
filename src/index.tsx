0e47a84b670fdfa8a538b5efe5b9a5a1f7fab95f
ohwoo-kwon <kwonow1@gmail.com>
2022-01-31 15:45:37 +0900

    8.1 - 8.2 Header part One & Two

--- src/index.tsx
+++ src/index.tsx
@@ -61,9 +61,9 @@ table {
 body {
   font-weight: 300;
   font-family: 'Source Sans Pro', sans-serif;
-  background:linear-gradient(135deg,#e09,#d0e);
   color:black;
   line-height: 1.2;
+  
 }
 a {
   text-decoration:none;
