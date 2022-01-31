9798dce224f2fd4e6019fdf6a08016f0da9257fa
ohwoo-kwon <kwonow1@gmail.com>
2022-01-31 14:59:12 +0900

    8.0 Introduction

--- src/atmos.tsx
+++ src/atmos.tsx
@@ -1,19 +1,1 @@
 import { atom } from "recoil";
-
-export interface IToDo {
-  id: number;
-  text: string;
-}
-
-interface IToDoState {
-  [key: string]: IToDo[];
-}
-
-export const toDoState = atom<IToDoState>({
-  key: "toDo",
-  default: {
-    "To Do": [],
-    Doing: [],
-    Done: [],
-  },
-});
--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom";
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
-import { darkTheme } from "./theme";
-
+import { theme } from "./theme";
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
@@ -75,7 +74,7 @@ a {
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
-      <ThemeProvider theme={darkTheme}>
+      <ThemeProvider theme={theme}>
         <GlobalStyle />
         <App />
       </ThemeProvider>
